f32222088e63a9f1fda65f885547f21896c5caec
faizisyellow <muhamatfaissalm@gmail.com>
2025-03-24 11:51:06 +0700

    opened connection to database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
@@ -19,6 +21,13 @@ func main() {
 		errorLog.Printf("can't load env file: %q", err)
 	}
 
+	db, err := openDB()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	db.Close()
+
 	mux := http.NewServeMux()
 	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome to todo-list web app."))
@@ -27,4 +36,18 @@ func main() {
 	infoLog.Printf("Starting server on %s", ADDRESS)
 	err = http.ListenAndServe(ADDRESS, mux)
 	errorLog.Fatal(err)
+}
+
+func openDB() (*sql.DB, error) {
+	dns := os.Getenv("DB_URL")
+
+	db, err := sql.Open("mysql", dns)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
