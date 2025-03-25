1493ad6cf009dad4948bc341dd276057a0440271
faizisyellow <muhamatfaissalm@gmail.com>
2025-03-25 18:42:19 +0700

    added templates

---
+++ ui/static/js/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+            },
+            colors: {
+                'light-blue': '#e0f7fa',
+                'blue-accent': '#4fc3f7',
+                'blue-dark': '#0288d1',
+            },
+        }
+    }
+}
